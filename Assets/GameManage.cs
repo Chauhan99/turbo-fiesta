977350ace7b7ff8dd45bea1aa22719d2771e9aa5
Swapnil Chauhan <sys.swapnil99@gmail.com>
2018-03-07 02:20:46 +0530

    First commit of all the project files of New2DGame

---
+++ Assets/GameManage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GameManage : MonoBehaviour {
+	public Questions[] questions;
+	private static List<Questions> unansweredQuestions;
+	private Questions currentQuestion;
+
+	void Start()
+	{
+		if (unansweredQuestions == null || unansweredQuestions.Count == 0) 
+		{
+			unansweredQuestions = questions.ToList<Questions>();
+			Debug.Log ("Haha " + unansweredQuestions.Count);
+		}
+		SetCurrentQuestion();
+		Debug.Log(currentQuestion.fact + "is" + currentQuestion.isTrue);	
+	}
+	void SetCurrentQuestion()
+	{
+		int randomQuestionIndex = Random.Range(0, unansweredQuestions.Count);
+		currentQuestion = unansweredQuestions[randomQuestionIndex];
+		unansweredQuestions.RemoveAt(randomQuestionIndex);
+	}
+
+}
